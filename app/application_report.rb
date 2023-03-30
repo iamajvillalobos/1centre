4818b9f4caf8dbb6532290e55c9c73557c871780
AJ Villalobos <hello@ajvillalobos.com>
2023-03-30 16:15:30 +0800

    Make the test pass

--- app/application_report.rb
+++ app/application_report.rb
@@ -1,7 +1,11 @@
-require 'json'
+require "json"
+require "date"
 
 class ApplicationReport
   def initialize(json_filename)
+    file = File.read("./applications.json")
+    @applications = JSON.parse(file, object_class: OpenStruct).applications
+    @trend = {}
   end
 
   #
@@ -13,6 +17,30 @@ class ApplicationReport
   #
   # Returns the average number of applications by hour (0 to 23) depending on
   # the total number of days available in the JSON file.
-  def retrieve_trend(channel = 'all')
+  def retrieve_trend(channel = "all")
+    filtered_by_channel = if channel == "all"
+      @applications
+    else
+      @applications.select { |app| app.channel == channel }
+    end
+
+    filtered_by_day = filtered_by_channel.group_by do |app|
+      Date.parse(app.timestamp)
+    end
+
+    filtered_by_day.each do |day, apps|
+      apps.each do |app|
+        datetime = DateTime.parse(app.timestamp)
+
+        @trend[datetime.hour] ||= 0
+        @trend[datetime.hour] += 1
+      end
+    end
+
+    @trend.each do |hour, value|
+      @trend[hour] = (value.to_f / filtered_by_day.size).round
+    end
+
+    @trend
   end
 end
